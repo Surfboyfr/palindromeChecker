dabe36425172e6b6437efa8f4ae930c36e296fd6
surfboyfr <praiserocky007@gmail.com>
2025-02-11 16:20:30 +0100

    correction

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ function checkPalindrome() {
      
    let str = document.getElementById("text-input").value;
    let resultText = document.getElementById("result");
-if(str===""){
-alert('Please input a value')
-}
+
    // Remove non-alphanumeric characters and convert to lowercase
    let cleanStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
    
    // Reverse the string and compare with the original
-   if (cleanStr === cleanStr.split("").reverse().join("")) {
+   if(str===""){
+      alert('Please input a value')
+      resultText.textContent = `Please input a value`;
+      }
+   else if (cleanStr === cleanStr.split("").reverse().join("")) {
        resultText.textContent = `${str} is a palindrome!`;
        resultText.style.color = "green";
    } else {
